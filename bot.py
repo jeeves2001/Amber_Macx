67c30f0ef191d8c3818c5437b0e91b19dfce4b9c
jeeves2001 <jamesstevens@outlook.com>
2017-05-05 11:08:33 +0100

    Basics

--- bot.py
+++ bot.py
@@ -1,5 +1,10 @@
+# Basic bot created using the Bot Framework provided by Molly White
+# Bot code: https://github.com/molly/twitterbot_framework
+# Website: http://blog.mollywhite.net
+
 from secrets import *
 
 auth = tweepy.OAuthHandler(C_KEY, C_SECRET)
 auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)
 api = tweepy.API(auth)
+
